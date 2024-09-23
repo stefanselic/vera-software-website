1302474ad9429b36c49db2e2e156db9c4cd5ce0a
stefanselic <s.stefan22@hotmail.com>
2024-09-23 20:13:39 +0200

    Add scroll effect for navigation bar

--- js/script.js
+++ js/script.js
@@ -12,3 +12,24 @@ if (checkReplace !== null) {
     autoRun: true,
   });
 }
+
+// User Scroll For Navbar
+function userScroll() {
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('bg-dark');
+      navbar.classList.add('border-bottom');
+      navbar.classList.add('border-secondary');
+      navbar.classList.add('navbar-sticky');
+    } else {
+      navbar.classList.remove('bg-dark');
+      navbar.classList.remove('border-bottom');
+      navbar.classList.remove('border-secondary');
+      navbar.classList.remove('navbar-sticky');
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', userScroll);
