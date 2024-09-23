bb622875a2b01f1e908265730ccd6481362defa9
stefanselic <s.stefan22@hotmail.com>
2024-09-23 22:26:34 +0200

    Add logic for video modal

--- js/script.js
+++ js/script.js
@@ -33,3 +33,28 @@ function userScroll() {
 }
 
 document.addEventListener('DOMContentLoaded', userScroll);
+
+// Video Modal
+const videoBtn = document.querySelector('.video-btn');
+const videoModal = document.querySelector('#videoModal');
+const video = document.querySelector('#video');
+let videoSrc;
+
+if (videoBtn !== null) {
+  videoBtn.addEventListener('click', () => {
+    videoSrc = videoBtn.getAttribute('data-bs-src');
+  });
+}
+
+if (videoModal !== null) {
+  videoModal.addEventListener('shown.bs.modal', () => {
+    video.setAttribute(
+      'src',
+      videoSrc + '?autoplay=1;modestbranding=1;showInfo=0',
+    );
+  });
+
+  videoModal.addEventListener('hide.bs.modal', () => {
+    video.setAttribute('src', videoSrc);
+  });
+}
